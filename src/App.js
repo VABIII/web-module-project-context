fbf276ce315601cdb4bfadc16ee89968be0cc4b8
VABIII <tbengtson82@gmail.com>
2021-12-10 18:32:31 -0600

    ProductContext built and providing info to product comp

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
+import ProductContext from "./contexts/ProductsContext";
 
 // Components
 import Navigation from './components/Navigation';
@@ -10,24 +11,27 @@ import ShoppingCart from './components/ShoppingCart';
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
+	console.log(cart)
 
 	const addItem = item => {
-		// add the given item to the cart
+		setCart([...cart, item])
 	};
 
 	return (
+		<ProductContext.Provider value={{products, addItem}}>
 		<div className="App">
 			<Navigation cart={cart} />
 
 			{/* Routes */}
 			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
+				<Products/>
 			</Route>
 
 			<Route path="/cart">
 				<ShoppingCart cart={cart} />
 			</Route>
 		</div>
+		</ProductContext.Provider>
 	);
 }
 
